782bc89907a6e3737d663d24847fac39c6e9eaa6
GLeb Tarakanov <glebtgd@gmail.com>
2023-03-23 21:17:56 +0600

    Создан проект инстаграмм

---
+++ accounts/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from accounts.manager import UserManager
+
+
+class Account(AbstractUser):
+    email = models.EmailField(verbose_name='Электронная почта', unique=True, blank=True)
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='user_pic',
+        verbose_name='Аватар'
+    )
+    birth_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата рождения'
+    )
+    liked_posts = models.ManyToManyField(
+        verbose_name='Понравившиеся публикации',
+        to='posts.Post',
+        related_name='user_likes'
+    )
+    subscriptions = models.ManyToManyField(
+        verbose_name='Подписки',
+        to='accounts.Account',
+        related_name='subscribers'
+    )
+    commented_posts = models.ManyToManyField(
+        verbose_name='Прокомментированные посты',
+        to='posts.Post',
+        related_name='user_comments'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
---
+++ posts/views.py
@@ -0,0 +1,42 @@
+from django.views.generic import ListView
+
+from posts.models import Post
+
+
+class IndexView(ListView):
+    template_name = 'task/tasks.html'
+
+    context_object_name = 'posts'
+    model = Post
+    ordering = ['-updated_at']
+
+    paginate_by = 9
+    paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_deleted=True)
+        if self.search_value:
+            query = Q(title__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
+
