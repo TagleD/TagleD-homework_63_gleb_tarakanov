3b20b6306c023d7ee01006a8d3cc8f6e6bb3e313
GLeb Tarakanov <glebtgd@gmail.com>
2023-03-27 20:19:52 +0600

    Доделан 4 этап. На главной странице теперь, если пользователь аутентифицирован, то показываются посты аккаунтов, на которые он подписан. Если таковых нет или не выполнен вход в аккаунт, то показываются посты всех кроме него самого

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
-from django.utils.http import urlencode
 from django.views.generic import ListView, CreateView
 
 from posts.forms import PostForm
@@ -16,34 +14,17 @@ class IndexView(ListView):
 
     context_object_name = 'posts'
     model = Post
-    ordering = ['-updated_at']
 
-    # def get(self, request, *args, **kwargs):
-    #     self.form = self.get_search_form()
-    #     self.search_value = self.get_search_value()
-    #     return super().get(request, *args, **kwargs)
-    #
-    # def get_search_form(self):
-    #     return SearchForm(self.request.GET)
-    #
-    # def get_search_value(self):
-    #     if self.form.is_valid():
-    #         return self.form.cleaned_data['search']
-    #     return None
-    #
-    # def get_queryset(self):
-    #     queryset = super().get_queryset().exclude(is_deleted=True)
-    #     if self.search_value:
-    #         query = Q(title__icontains=self.search_value) | Q(author__icontains=self.search_value)
-    #         queryset = queryset.filter(query)
-    #     return queryset
-    #
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(object_list=object_list, **kwargs)
-    #     context['form'] = self.form
-    #     if self.search_value:
-    #         context['query'] = urlencode({'search': self.search_value})
-    #     return context
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            posts = Post.objects.filter(author__in=self.request.user.subscriptions.all()).order_by('created_at')
+            if posts:
+                return posts
+            else:
+                return Post.objects.all().order_by('created_at').exclude(author=self.request.user)
+        else:
+            return Post.objects.all().order_by('created_at').exclude(author=self.request.user)
+
 
 class CreatePostView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     template_name = 'post_create.html'
@@ -60,4 +41,3 @@ class CreatePostView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
         post.author = self.request.user
         post.save()
         return redirect('index')
-
